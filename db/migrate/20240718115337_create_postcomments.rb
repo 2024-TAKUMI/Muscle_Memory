06cbf072eaa45e349088da6283f38f029020bab6
2024-TAKUMI <montakumi36@gmail.com>
2024-07-24 10:56:13 +0000

    update migration

--- db/migrate/20240718115337_create_postcomments.rb
+++ db/migrate/20240718115337_create_postcomments.rb
@@ -3,7 +3,7 @@ class CreatePostcomments < ActiveRecord::Migration[6.1]
     create_table :postcomments do |t|
 
       t.text :comment, null: false # コメント本文
-      t.integer :post_image_id, null: false, default: "" # コメントされた投稿画像のid
+      t.integer :post_image_id, null: false # コメントされた投稿画像のid
       
       t.timestamps
     end
