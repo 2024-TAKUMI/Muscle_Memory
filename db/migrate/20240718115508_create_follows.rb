b08f9b16492a2f5534c61b3ce6bf61263b0e5e8d
2024-TAKUMI <montakumi36@gmail.com>
2024-07-24 10:57:20 +0000

    update migration

--- db/migrate/20240718115508_create_follows.rb
+++ db/migrate/20240718115508_create_follows.rb
@@ -2,8 +2,8 @@ class CreateFollows < ActiveRecord::Migration[6.1]
   def change
     create_table :follows do |t|
 
-      t.integer :followers_id, null: false, default: "" # フォロワーid
-      t.integer :followee_id, null: false, default: "" # フォロイーid
+      t.integer :followers_id, null: false # フォロワーid
+      t.integer :followee_id, null: false # フォロイーid
       
       t.timestamps
     end
